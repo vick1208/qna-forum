2f748cc1ac51b5c4ba7b67b8bf448aa3cba01ff8
Vicky AS <vickyhuang808@gmail.com>
2024-04-24 20:45:19 +0700

    membuat navbar

--- forum-tanya-jawab/resources/views/partials/nav.blade.php
+++ forum-tanya-jawab/resources/views/partials/nav.blade.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Forum Kita</title>
-</head>
-<body>
-
-</body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ddd3d3;">
+    <a class="navbar-brand" href="/">Forum Kita</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+        </ul>
+    </div>
+</nav>
