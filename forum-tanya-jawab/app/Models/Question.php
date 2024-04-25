2c7b63064973c5d7ec5c61dd1e7a18b7045a4ab2
Vicky AS <vickyhuang808@gmail.com>
2024-04-25 19:51:30 +0700

    install eloquent sluggable

--- forum-tanya-jawab/app/Models/Question.php
+++ forum-tanya-jawab/app/Models/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Question extends Model
 {
     use HasFactory;
+    use Sluggable;
     protected $guarded = ['id'];
 
     public function user(): BelongsTo
@@ -24,5 +26,14 @@ class Question extends Model
     public function replies(): HasMany
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- forum-tanya-jawab/database/migrations/2024_04_24_070320_create_questions_table.php
+++ forum-tanya-jawab/database/migrations/2024_04_24_070320_create_questions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('detail');
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('user_id');
